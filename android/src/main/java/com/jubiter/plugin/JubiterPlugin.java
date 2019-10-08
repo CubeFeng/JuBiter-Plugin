1db6b67dcca4358c1a2c09983a723c62fdb465f0
Cube <724790472@qq.com>
2019-10-08 16:00:47 +0800

    1. plugin add aar file 2. update project coonfiguration

--- android/src/main/java/com/jubiter/plugin/JubiterPlugin.java
+++ android/src/main/java/com/jubiter/plugin/JubiterPlugin.java
@@ -6,20 +6,24 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** JubiterPlugin */
+/**
+ * JubiterPlugin
+ */
 public class JubiterPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "plugin");
-    channel.setMethodCallHandler(new JubiterPlugin());
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "plugin");
+        channel.setMethodCallHandler(new JubiterPlugin());
     }
-  }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
+    }
 }
